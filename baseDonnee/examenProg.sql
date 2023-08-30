d918182184a53f6293d57039755b25d45bb0c741
Idealy <123444169+junkaM@users.noreply.github.com>
2023-08-30 15:23:19 +0300

    feat: update scripts for the database

--- baseDonnee/examenProg.sql
+++ baseDonnee/examenProg.sql
@@ -9,14 +9,7 @@ CREATE TABLE Client (
     prenom VARCHAR(100),
     adresse VARCHAR(50),
     numero_telephone VARCHAR(20)
-);   
-
-CREATE TABLE liste_commande (
-    liste_commande_id SERIAL PRIMARY KEY,
-    quantite INT,
-    produit_id_produit INT,
-    FOREIGN KEY (produit_id_produit) REFERENCES produit(produit_id)
-);
+); 
 
 CREATE TABLE produit (
     produit_id SERIAL PRIMARY KEY,
@@ -24,6 +17,13 @@ CREATE TABLE produit (
     description TEXT,
     prix_unitaire DOUBLE PRECISION,
     stock INT
+);
+
+CREATE TABLE liste_commande (
+    liste_commande_id SERIAL PRIMARY KEY,
+    quantite INT,
+    produit_id_produit INT,
+    FOREIGN KEY (produit_id_produit) REFERENCES produit(produit_id)
 );
 
 CREATE TABLE commande (
@@ -50,17 +50,17 @@ VALUES ('Doe', 'John', '123 Main St', '555-1234'),
        ('Smith', 'Jane', '456 Elm St', '555-5678'),
        ('Johnson', 'Michael', '789 Oak St', '555-9876');
 
--- Inserts pour la table liste_commande
-INSERT INTO liste_commande (quantite, produit_id_produit)
-VALUES (2, 1),
-       (3, 2),
-       (1, 3);
-
 -- Inserts pour la table produit
 INSERT INTO produit (nom, description, prix_unitaire, stock)
 VALUES ('Eau minérale', 'Eau minérale naturelle', 1.50, 100),
        ('Café', 'Café arabica torréfié', 3.00, 50),
        ('Soda', 'Soda gazeux saveur cola', 2.00, 75);
+
+-- Inserts pour la table liste_commande
+INSERT INTO liste_commande (quantite, produit_id_produit)
+VALUES (2, 1),
+       (3, 2),
+       (1, 3);
 
 -- Inserts pour la table commande
 INSERT INTO commande (liste_commande_id, date_de_commande, client_id_Client, liste_commande_id_liste_commande)
